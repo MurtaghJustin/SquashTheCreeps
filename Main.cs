26485e7216b03e9d62406baeb27857af5a291656
Justin Murtagh <justin.murtagh@carsales.com.au>
2025-01-11 12:16:11 +1100

    We can have another go!

--- Main.cs
+++ Main.cs
@@ -9,13 +9,20 @@ public partial class Main : Node
 	private Player Player;
 	private PathFollow3D MobSpawnLocation;
 	private Timer MobTimer;
+	private ScoreLabel UIScoreLabel;
+	private Control UIRetry;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Setup node references
 		Player = GetNode<Player>("Player");
 		MobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
 		MobTimer = GetNode<Timer>("MobTimer");
+		UIScoreLabel = GetNode<ScoreLabel>("UI/ScoreLabel");
+		UIRetry = GetNode<Control>("UI/Retry");
+
+		UIRetry.Hide();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,11 +38,19 @@ public partial class Main : Node
 
 		Vector3 playerPosition = Player.Position;
 		mob.Initialize(spawnLoc.Position, playerPosition);
+		mob.Squashed += UIScoreLabel.OnMobSquashed;
 		AddChild(mob);
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed("ui_accept") && UIRetry.Visible)
+			GetTree().ReloadCurrentScene();
 	}
 
 	private void OnPlayerHit()
 	{
 		MobTimer.Stop();
+		UIRetry.Show();
 	}
 }
---
+++ ScoreLabel.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class ScoreLabel : Label
+{
+	private int _score = 0;
+	private string ScoreText
+	{
+		get
+		{
+			return $"Squishes: {_score:D2}";
+		}
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnMobSquashed()
+	{
+		_score++;
+		Text = ScoreText;
+	}
+}
